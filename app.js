c3359fed92a5ee1240260733c43743e9b29ab20a
MyNameIsSergey <48844295+MyNameIsSergey@users.noreply.github.com>
2021-03-29 23:17:39 +0300

    changed static folder

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static(__dirname + "/client/public"));
+app.use(express.static(__dirname + "/client/build"));
 
 app.listen(PORT);
