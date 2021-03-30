8625a107cd71330d10c1a1da6a4f0eb170b59543
BlockiyDmitriy <51110104+BlockiyDmitriy@users.noreply.github.com>
2021-03-30 20:24:48 +0300

    Add output

--- client/src/components/Display.js
+++ client/src/components/Display.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { NumberContext } from './NumberProvider';
 
 const Display = () => {
-    return (
-        <div>
-
-        </div>
-    );
+  const { number } = useContext(NumberContext);
+  return (
+    <div>
+      <h2>{number}</h2>
+      <p>Enter Some Numbers</p>
+    </div>
+  );
 };
 
 export default Display;
