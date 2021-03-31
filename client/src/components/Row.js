6c5d09662c3936436ec0f69dad715dc1685505e8
MyNameIsSergey <48844295+MyNameIsSergey@users.noreply.github.com>
2021-03-31 20:10:27 +0300

    fix: typo in 'props.children'

--- client/src/components/Row.js
+++ client/src/components/Row.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Row = (props) => {
     return (
         <div className="row">
-            {props.childeren}
+            {props.children}
         </div>
     );
 }
