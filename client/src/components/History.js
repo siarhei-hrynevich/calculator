69fcff352b4394b516215ca8808cebd33d63b4d8
MyNameIsSergey <48844295+MyNameIsSergey@users.noreply.github.com>
2021-04-12 22:39:27 +0300

    added history fetch

--- client/src/components/History.js
+++ client/src/components/History.js
@@ -5,11 +5,29 @@ import { useState } from 'react'
 const History = () => {
     const [show, setShow] = useState(false);
     const [history, setHistory] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        if (show === false || isLoaded) return;
+        fetch("api/history/all")
+            .then(result => result.json())
+            .then(
+                (result) => {
+                    console.log(result);
+                    setHistory(result);
+                    setIsLoaded(true);
+                },
+                (error) => {
+                    console.log(error);
+                    setIsLoaded(false);
+                }
+            );
+    });
 
     return (
         <div>
             <Popup title='History' show={show} onClose={() => setShow(false)}>
-                {history.map((item) => (<div>{item}</div>))}
+                {history.map((item) => (<div key={item.id}>{item.operation}</div>))}
             </Popup>
             <button onClick={() => setShow(true)}>Show History</button>
         </div>
